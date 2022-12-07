fc0e44c5e98dcaa0b47f1edc9a4e738cbd2e1ad0
IrasemaSJ <Irasema.Serrano@improving.com>
2022-12-07 16:03:13 -0600

    New routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomeScreen from './pages/HomeScreen';
 import './App.css';
+import CustomizeScreen from './pages/CustomizeScreen';
+import GameScreen from './pages/GameScreen';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<HomeScreen/>}/>
+        <Route path='/customize' element={<CustomizeScreen/>}/>
+        <Route path='/game' element={<GameScreen/>}/>
       </Routes>
     </BrowserRouter>
   );
