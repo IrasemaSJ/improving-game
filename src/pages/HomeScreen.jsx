19ea2e94cd4e3cad5a2a3ff9d1472dbabd1be8c4
IrasemaSJ <Irasema.Serrano@improving.com>
2022-12-07 17:08:13 -0600

    Adding title, button and history

--- src/pages/HomeScreen.jsx
+++ src/pages/HomeScreen.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
+import { IoAddSharp } from "react-icons/io5";
 
 //Importar estilos
 import '../theme/HomeTheme.css'
 
 const HomeScreen = () => {
   return (
-    <div
-        className='container'
-    >
-        
+    <div className='container' >
+      <div>
+      <h1 className='title'>Improving Game</h1> 
+      <div style={{justifyContent: 'flex-end', width: '100%', display: 'flex'}}> 
+        <button className='btnAdd'><IoAddSharp/></button> 
+      </div> 
+
+      <div className='containerHistory'> ufdi </div> 
+      </div>
+
+
+
     </div>
   )
 }
